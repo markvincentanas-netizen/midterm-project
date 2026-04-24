7b17e5134ed6412011c150588b926f6d2a09b22c
markvincentanas-netizen <markvincentanas@gmail.com>
2026-04-24 09:04:52 +0800

    Add files via upload

---
+++ student.js
@@ -0,0 +1,12 @@
+// VARIABLES
+let name = "Mark Vincent Anas";
+let course = "BSIT";
+let yearLevel = "3rd Year";
+let favoriteLanguage = "JavaScript";
+
+// OUTPUT
+console.log("===== STUDENT INFORMATION =====");
+console.log("Name: " + name);
+console.log("Course: " + course);
+console.log("Year Level: " + yearLevel);
+console.log("Favorite Programming Language: " + favoriteLanguage);
